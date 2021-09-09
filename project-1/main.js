5718bb865b7f5a676b24b4c18e106d8d4c6a74f4
angkyle <60246866+angkyle@users.noreply.github.com>
2021-09-08 19:03:35 -0700

    main js added

---
+++ project-1/main.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+  $(".deer01").draggable();
+  $(".deer02").draggable();
+  $(".deer03").draggable();
+  $(".deer04").draggable();
+  $(".floatComp").click(function () {
+    $(this).toggle(1300);
+  });
+});
