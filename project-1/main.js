311ccf7bf6f547f8b3d5f1432bc6a28f2d62fe8a
angkyle <60246866+angkyle@users.noreply.github.com>
2021-09-09 12:56:04 -0700

    changed bg, added imgs, added js

--- project-1/main.js
+++ project-1/main.js
@@ -1,9 +1,37 @@
 $(document).ready(function () {
-  $(".deer01").draggable();
-  $(".deer02").draggable();
-  $(".deer03").draggable();
-  $(".deer04").draggable();
-  $(".floatComp").click(function () {
-    $(this).toggle(1300);
+  $(function () {
+    $(
+      "#flower01, #flower02, #flower03, #flower04, #flower05, #flower06, #deer01, #deer02, #deer03, #deer04, #tardigrade, #mri, #laptop01, #laptop02, #glitch01, #neuron"
+    ).draggable();
+  });
+
+  $("#p1").click(function () {
+    $(".pop-up").toggle();
   });
 });
+
+$("#p2").hover(
+  function () {
+    $("#p3").fadeIn();
+  },
+  function () {
+    $("#p3").fadeOut();
+  }
+);
+
+// var bgImg = [
+//   "url(asset/sunset.gif)",
+//   "url(asset/portal.gif)",
+//   "url(asset/plants01.gif)",
+// ];
+
+// var counter = 0;
+
+// $("#garden-option").click(function () {
+//   if (counter == bgImg.length) {
+//     counter = 0;
+//   }
+//   var currentBg = bgImg[counter];
+//   $("body").css("background-image", currentBg);
+//   counter++;
+// });
